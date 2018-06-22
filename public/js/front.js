4897e208a9fa2bc43f7cfc3222f4da3c6abf4cf1
Alex Trilling <atrill90@gmail.com>
2018-06-22 15:50:39 -0400

    perfecting project card animations

--- public/js/front.js
+++ public/js/front.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+});
