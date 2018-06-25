8f46eaff565c0ad0085c40e2e1eb87325b852159
Alex Trilling <atrill90@gmail.com>
2018-06-25 12:35:40 -0400

    looping through json file to grab projects, need to add tech stack to json

--- public/js/front.js
+++ public/js/front.js
@@ -3,7 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Materialbox.init(elems);
   });
 
+//collapsible
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
 });
+
+
+//tool tip
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+});
+
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-
+const project_data = require("../projects.json");
 
 router.get('/', (req, res) => {
-    res.render("index");
-  })
+    res.render("index",{projects:project_data});
+  });
 
   module.exports = router;
